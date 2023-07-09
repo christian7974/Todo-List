1c523e99e86719ed24c2004c655ab0c4706ad00e
christian7974 <ctropeano@uri.edu>
2023-07-09 15:56:25 -0400

    Changed Structure of List Items
    Changed the structure of the list nodes to have a label associate with the input.

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const addItemButton = document.getElementById("btn-add-item");
 var itemToAdd = document.getElementById("item-to-add");
 var listOfItems = document.getElementById("list-of-items");
+var uniqueID = 1;
+
 var addItemToList = function() {
     var newListItem = document.createElement("li");
     var checkboxForItem = document.createElement("input");
     checkboxForItem.type = "checkbox";
-    newListItem.appendChild(checkboxForItem);
+    checkboxForItem.id = uniqueID.toString();
+    var labelForItem = document.createElement("label");
     var textInItem = document.createTextNode(itemToAdd.value);
-    newListItem.appendChild(textInItem);
+    labelForItem.appendChild(textInItem);
+    labelForItem.for = uniqueID;
+    newListItem.appendChild(checkboxForItem);
+    newListItem.appendChild(labelForItem);
     listOfItems.appendChild(newListItem);
+    uniqueID++;
+    itemToAdd.value = "";
 }
+
 addItemButton.addEventListener('click', addItemToList);
 itemToAdd.addEventListener("keypress", (event) => {
     if (event.key == 'Enter') {
         addItemToList();
     }
-})
+});
+var checkboxArray = document.querySelectorAll("input");
+var addListeners = function() {
+    
+};
