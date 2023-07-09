21b268ed5ea5acbac2e2cbecdec44fac919e382c
christian7974 <ctropeano@uri.edu>
2023-07-09 16:23:36 -0400

    Added Styling, Clear List and Crossing Out
    Added some very simple styling, the ability to cross something out and have it disappear as well as the ability to clear an entire list.

--- index.js
+++ index.js
@@ -2,30 +2,49 @@ const addItemButton = document.getElementById("btn-add-item");
 var itemToAdd = document.getElementById("item-to-add");
 var listOfItems = document.getElementById("list-of-items");
 var uniqueID = 1;
-
+const clearListButton = document.getElementById("btn-clear-list");
 var addItemToList = function() {
-    var newListItem = document.createElement("li");
-    var checkboxForItem = document.createElement("input");
-    checkboxForItem.type = "checkbox";
-    checkboxForItem.id = uniqueID.toString();
-    var labelForItem = document.createElement("label");
-    var textInItem = document.createTextNode(itemToAdd.value);
-    labelForItem.appendChild(textInItem);
-    labelForItem.for = uniqueID;
-    newListItem.appendChild(checkboxForItem);
-    newListItem.appendChild(labelForItem);
-    listOfItems.appendChild(newListItem);
-    uniqueID++;
-    itemToAdd.value = "";
+    if (itemToAdd.value != "") { // Prevents user from adding blank items to the list
+        var newListItem = document.createElement("li");
+        var checkboxForItem = document.createElement("input");
+        checkboxForItem.type = "checkbox";
+        checkboxForItem.id = uniqueID.toString();
+        checkboxForItem.className = "list-checkbox"
+        var labelForItem = document.createElement("label");
+        var textInItem = document.createTextNode(itemToAdd.value);
+        labelForItem.appendChild(textInItem);
+        newListItem.id = uniqueID;
+        labelForItem.for = uniqueID;
+
+        newListItem.appendChild(checkboxForItem);
+        newListItem.appendChild(labelForItem);
+        listOfItems.appendChild(newListItem);
+        uniqueID++;
+        checkboxForItem.addEventListener('click', () => {
+            labelForItem.classList.toggle("crossed-out");
+            setTimeout(() => {
+                newListItem.remove();
+            }, "4000");
+            
+        });
+        itemToAdd.value = "";
+    }
+   
+}
+
+var clearList = function() {
+    var allItems = document.querySelectorAll("li");
+    var numDone = 0;
+    for (var i = 0; i < allItems.length; i++) {
+        allItems[i].remove();
+    }
 }
 
 addItemButton.addEventListener('click', addItemToList);
+clearListButton.addEventListener('click', clearList);
 itemToAdd.addEventListener("keypress", (event) => {
     if (event.key == 'Enter') {
         addItemToList();
     }
 });
 var checkboxArray = document.querySelectorAll("input");
-var addListeners = function() {
-    
-};
